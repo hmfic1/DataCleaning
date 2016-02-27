e44db6045833d4c084cb9648b2202de6986ffc8d
hmfic1 <jimrmck@gmail.com>
2016-02-26 20:35:32 -0800

    version 2016 02 26 ver 2

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,12 @@
 ##  run_analysis.R
 
-##    setwd("/Users/Jim/DataCleaning")
+##    set working directory
 setwd("~")
 
 ##    load appropriate packages
 
 library(plyr)
 library(dplyr)
-
-##    set working directory
-##    
-
-##    set the base working directory
-##    basewd <- getwd()
-##    setwd(basewd)
 
 ##    make sure that directories are set up correctly
 
@@ -29,45 +22,35 @@ if(dir.exists("UCI HAR Dataset")) {
 
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "data.zip")
 unzip("data.zip")
-setwd("UCI HAR Dataset")
-##      features <- read.table("features.txt")
-##      activityLabels <- read.table("activity_labels.txt")
 
 features <-read.table("~/UCI HAR Dataset/features.txt")
 activityLabels <- read.table("~/UCI HAR Dataset/activity_labels.txt")
 
-##      setwd("test")
-
-##      XTestfile <- read.table("X_test.txt", strip.white = TRUE)
 XTestfile <- read.table("~/UCI HAR Dataset/test/X_test.txt", strip.white = TRUE)
-##      SubjectTestFile <- read.table("subject_test.txt")
 SubjectTestFile <- read.table("~/UCI HAR Dataset/test/subject_test.txt")
-##      ActivityTestFile <- read.table("y_test.txt")
 ActivityTestFile <- read.table("~/UCI HAR Dataset/test/y_test.txt")
-
-##      setwd(basewd)
-##      setwd("UCI HAR Dataset/train")
-
-##      Xtrainfile <- read.table("X_train.txt", strip.white = TRUE)
-##      SubjecttrainFile <- read.table("subject_train.txt")
-##      ActivitytrainFile <- read.table("y_train.txt")
 
 XTrainfile <- read.table("~/UCI HAR Dataset/train/X_train.txt", strip.white = TRUE)
 SubjectTrainFile <- read.table("~/UCI HAR Dataset/train/subject_train.txt")
 ActivityTrainFile <- read.table("~/UCI HAR Dataset/train/y_train.txt")
 
-head(names(Xtrainfile), 20)
-head(Xtrainfile[1:3])
-class(Xtrainfile$V1)
-
-head(SubjecttrainFile)
-head(ActivitytrainFile)
-table(SubjecttrainFile)
-table(ActivitytrainFile)
-
-head(names(XTestfile), 3)
 head(names(XTestfile), 20)
 head(XTestfile[1:3])
+class(XTestfile$V1)
+
+head(names(XTrainfile), 20)
+head(XTrainfile[1:3])
+class(XTrainfile$V1)
+
+head(SubjectTestFile)
+head(ActivityTestFile)
+table(SubjectTestFile)
+table(ActivityTestFile)
+
+head(SubjectTrainFile)
+head(ActivityTrainFile)
+table(SubjectTrainFile)
+table(ActivityTrainFile)
 
 class(XTestfile$V1)
 
@@ -86,15 +69,20 @@ head(XTest2[1:10])
 
 ##  remove the unneeded columns
 
-v <- as.character(features$V2)
 colnames(XTest2) <- c("Subject", "Activity", as.character(features$V2))
 
+stdColumns <- grep(".std\\().", features$V2)
+stdColumnNames <- grep(".std\\().", features$V2, value = TRUE)
+meanColumns <- grep(".mean\\().", features$V2)
+meanColumnNames <- grep(".mean\\().", features$V2, value = TRUE)
 
-
-grep
-
-grep(".std\\().", features$V2, value = TRUE)
-grep(".mean\\().", features$V2, value = TRUE)
+XTestSelectedstd <- XTestfile[,stdColumns]
+colnames(XTestSelectedstd) <- stdColumnNames
+XTestSelectedmean <- XTestfile[,meanColumns]
+colnames(XTestSelectedmean) <- meanColumnNames
+XTest0 <- cbind(XTestSelectedstd, XTestSelectedmean)
+XTest1 <- cbind(Activity = ActivityTestFile, XTest0)
+Xtest2 <- cbind(Subject = SubjectTestFile, XTest1)
 
 ##  contains(x, ignore.case = TRUE): selects all variables whose name contains x
 head(select(XTest2, contains(".mean\\().")))
