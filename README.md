c7b084eb9497db8a9d562dd4aa50e11336ea5eae
hmfic1 <jimrmck@gmail.com>
2016-02-27 13:40:04 -0800

    raw finished script 2016 02 27

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# DataCleaning
-Coursera course on Getting and Cleaning Data
+# Getting and Cleaning Data
+## Final Project
+### February, 2016
+
+
+Before running the run_analysis.R script:
+  
+* ensure that you have an internet connection established
+* open R or R Studio
+* set your working directory to the directory of your choice
+    + This script will download a zip file to your current working directory
+* ensure that you have the following packages installed: plyr, dplyr, tidyr
+* open the run_analysis.R script in R or R Studio
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,27 @@
 ##  run_analysis.R
 
-##    set working directory
-setwd("~")
-
 ##    load appropriate packages
 
 library(plyr)
 library(dplyr)
+library(tidyr)
 
-##    make sure that directories are set up correctly
+##    delete any previous files and directories
 
-if(dir.exists("data.zip")) {
+if(file.exists("SamsungFile.txt")) {
+  file.remove("SamsungFile.txt")
+}
+
+if(file.exists("SamsungFile2.txt")) {
+  file.remove("SamsungFile2.txt")
+}
+
+if(file.exists("data.zip")) {
   file.remove("data.zip")
 }
 
 if(dir.exists("UCI HAR Dataset")) {
-  unlink("UCI HAR Dataset")
+  unlink("UCI HAR Dataset", recursive = TRUE)
 }
 
 ##    download and unzip data files
@@ -23,79 +29,103 @@ if(dir.exists("UCI HAR Dataset")) {
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "data.zip")
 unzip("data.zip")
 
-features <-read.table("~/UCI HAR Dataset/features.txt")
-activityLabels <- read.table("~/UCI HAR Dataset/activity_labels.txt")
+##    read files into datasets
 
-XTestfile <- read.table("~/UCI HAR Dataset/test/X_test.txt", strip.white = TRUE)
-SubjectTestFile <- read.table("~/UCI HAR Dataset/test/subject_test.txt")
-ActivityTestFile <- read.table("~/UCI HAR Dataset/test/y_test.txt")
+features <-read.table("./UCI HAR Dataset/features.txt")
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-XTrainfile <- read.table("~/UCI HAR Dataset/train/X_train.txt", strip.white = TRUE)
-SubjectTrainFile <- read.table("~/UCI HAR Dataset/train/subject_train.txt")
-ActivityTrainFile <- read.table("~/UCI HAR Dataset/train/y_train.txt")
+XTestfile <- read.table("./UCI HAR Dataset/test/X_test.txt", strip.white = TRUE)
+SubjectTestFile <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+ActivityTestFile <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
-head(names(XTestfile), 20)
-head(XTestfile[1:3])
-class(XTestfile$V1)
+XTrainfile <- read.table("./UCI HAR Dataset/train/X_train.txt", strip.white = TRUE)
+SubjectTrainFile <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+ActivityTrainFile <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
-head(names(XTrainfile), 20)
-head(XTrainfile[1:3])
-class(XTrainfile$V1)
+##     get Activity names into Activity files, then replace variable names
 
-head(SubjectTestFile)
-head(ActivityTestFile)
-table(SubjectTestFile)
-table(ActivityTestFile)
+ActivityTestIdx <- merge(ActivityTestFile, activityLabels)
+colnames(ActivityTestIdx) <- c("Activity Number", "Activity")
+ActivityTrainIdx <- merge(ActivityTrainFile, activityLabels)
+colnames(ActivityTrainIdx) <- c("Activity Number", "Activity")
 
-head(SubjectTrainFile)
-head(ActivityTrainFile)
-table(SubjectTrainFile)
-table(ActivityTrainFile)
+##     replace variable name in Subject files
 
-class(XTestfile$V1)
+colnames(SubjectTestFile) <- c("Subject")
+colnames(SubjectTrainFile) <- c("Subject")
 
-
-XTrain1 <- cbind(ActivitytrainFile, Xtrainfile)
-Xtrain2 <- cbind(SubjecttrainFile, XTrain1)
-head(Xtrain2[1:10])
-
-XTest1 <- cbind(ActivityTestFile, XTestfile)
-XTest2 <- cbind(SubjectTestFile, XTest1)
-head(XTest2[1:10])
-
-##  merge the two data sets
-
-
-
-##  remove the unneeded columns
-
-colnames(XTest2) <- c("Subject", "Activity", as.character(features$V2))
+##     get columns and column names for mean() and std()
 
 stdColumns <- grep(".std\\().", features$V2)
 stdColumnNames <- grep(".std\\().", features$V2, value = TRUE)
 meanColumns <- grep(".mean\\().", features$V2)
 meanColumnNames <- grep(".mean\\().", features$V2, value = TRUE)
 
+##     subset test and training datasets, getting only the mean() and std() columns, and replace variable names
+
 XTestSelectedstd <- XTestfile[,stdColumns]
 colnames(XTestSelectedstd) <- stdColumnNames
 XTestSelectedmean <- XTestfile[,meanColumns]
 colnames(XTestSelectedmean) <- meanColumnNames
+
+XTrainSelectedstd <- XTrainfile[,stdColumns]
+colnames(XTrainSelectedstd) <- stdColumnNames
+XTrainSelectedmean <- XTrainfile[,meanColumns]
+colnames(XTrainSelectedmean) <- meanColumnNames
+
+#    merge the mean() and std() datasets, and add the activity and subject variables
+
 XTest0 <- cbind(XTestSelectedstd, XTestSelectedmean)
-XTest1 <- cbind(Activity = ActivityTestFile, XTest0)
-Xtest2 <- cbind(Subject = SubjectTestFile, XTest1)
+XTest1 <- cbind(ActivityTestIdx["Activity"], XTest0)
+XTest2 <- cbind(SubjectTestFile, XTest1)
 
-##  contains(x, ignore.case = TRUE): selects all variables whose name contains x
-head(select(XTest2, contains(".mean\\().")))
+XTrain0 <- cbind(XTrainSelectedstd, XTrainSelectedmean)
+XTrain1 <- cbind(ActivityTrainIdx["Activity"], XTrain0)
+XTrain2 <- cbind(SubjectTrainFile, XTrain1)
 
+##  merge the two data sets
 
-##  replace variable names
-
-
-
-##  melt? data frame
+FinalData <- rbind(XTest2, XTrain2)
 
 
+##  gather, then separate
+
+xx <- FinalData %>%
+  gather(Measure, Measurement, `tBodyAcc-std()-X`:`fBodyGyro-mean()-Z`)
+yy <- xx %>%
+  separate(Measure, into=c("ActType", "MeasureType", "Axis"), sep ="\\-")
+zz <- yy %>%
+  group_by(Subject, Activity, ActType, MeasureType, Axis)
+qq <- zz %>%
+  summarize(AvgMeasure = mean(Measurement))
+
+rr <- xx %>%
+  group_by(Subject, Activity, Measure)
+ss <- rr %>%
+  summarize(AvgMeasure = mean(Measurement))
+
+qq <- arrange(qq, Subject, Activity, ActType, MeasureType, Axis)
+ss <- arrange(ss, Subject, Activity, Measure)
+
+##    Initial Cleanup
+
+##    Delete downloaded files
+
+file.remove("data.zip")
+unlink("UCI HAR Dataset", recursive = TRUE)
+
+##    Write output file
+
+write.table(qq, file = "SamsungFile.txt", sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(ss, file = "SamsungFile2.txt", sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+##    Remove objects from environment
+
+##    rm(list=ls())
+
+print("End of Script")
 
 
+##    Check your directory for the file "SamsungFile.txt
 
-
+##    End
