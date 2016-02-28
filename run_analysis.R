5910335b255bebe2cd9cf4ab150416b4836b409e
hmfic1 <jimrmck@gmail.com>
2016-02-27 17:19:12 -0800

    fixed script 2016 02 27

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 
 ##    load appropriate packages
 
-library(plyr)
+library(plyr) 
 library(dplyr)
 library(tidyr)
 
@@ -10,10 +10,6 @@ library(tidyr)
 
 if(file.exists("SamsungFile.txt")) {
   file.remove("SamsungFile.txt")
-}
-
-if(file.exists("SamsungFile2.txt")) {
-  file.remove("SamsungFile2.txt")
 }
 
 if(file.exists("data.zip")) {
@@ -42,18 +38,6 @@ XTrainfile <- read.table("./UCI HAR Dataset/train/X_train.txt", strip.white = TR
 SubjectTrainFile <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 ActivityTrainFile <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
-##     get Activity names into Activity files, then replace variable names
-
-ActivityTestIdx <- merge(ActivityTestFile, activityLabels)
-colnames(ActivityTestIdx) <- c("Activity Number", "Activity")
-ActivityTrainIdx <- merge(ActivityTrainFile, activityLabels)
-colnames(ActivityTrainIdx) <- c("Activity Number", "Activity")
-
-##     replace variable name in Subject files
-
-colnames(SubjectTestFile) <- c("Subject")
-colnames(SubjectTrainFile) <- c("Subject")
-
 ##     get columns and column names for mean() and std()
 
 stdColumns <- grep(".std\\().", features$V2)
@@ -65,46 +49,60 @@ meanColumnNames <- grep(".mean\\().", features$V2, value = TRUE)
 
 XTestSelectedstd <- XTestfile[,stdColumns]
 colnames(XTestSelectedstd) <- stdColumnNames
+
 XTestSelectedmean <- XTestfile[,meanColumns]
 colnames(XTestSelectedmean) <- meanColumnNames
 
 XTrainSelectedstd <- XTrainfile[,stdColumns]
 colnames(XTrainSelectedstd) <- stdColumnNames
+
 XTrainSelectedmean <- XTrainfile[,meanColumns]
 colnames(XTrainSelectedmean) <- meanColumnNames
 
-#    merge the mean() and std() datasets, and add the activity and subject variables
+#    
+
+yy <- cbind(SubjectTestFile, ActivityTestFile)
+colnames(yy) <- c("Subject", "Activity")
+
+yy$Activity <- as.character(yy$Activity)
+yy$Activity[yy$Activity == "1"] <- "WALKING"
+yy$Activity[yy$Activity == "2"] <- "WALKING_UPSTAIRS"
+yy$Activity[yy$Activity == "3"] <- "WALKING_DOWNSTAIRS"
+yy$Activity[yy$Activity == "4"] <- "SITTING"
+yy$Activity[yy$Activity == "5"] <- "STANDING"
+yy$Activity[yy$Activity == "6"] <- "LAYING"
 
 XTest0 <- cbind(XTestSelectedstd, XTestSelectedmean)
-XTest1 <- cbind(ActivityTestIdx["Activity"], XTest0)
-XTest2 <- cbind(SubjectTestFile, XTest1)
+XTest1 <- cbind(yy, XTest0)
+
+zz <- cbind(SubjectTrainFile, ActivityTrainFile)
+colnames(zz) <- c("Subject", "Activity")
+
+zz$Activity <- as.character(zz$Activity)
+zz$Activity[zz$Activity == "1"] <- "WALKING"
+zz$Activity[zz$Activity == "2"] <- "WALKING_UPSTAIRS"
+zz$Activity[zz$Activity == "3"] <- "WALKING_DOWNSTAIRS"
+zz$Activity[zz$Activity == "4"] <- "SITTING"
+zz$Activity[zz$Activity == "5"] <- "STANDING"
+zz$Activity[zz$Activity == "6"] <- "LAYING"
 
 XTrain0 <- cbind(XTrainSelectedstd, XTrainSelectedmean)
-XTrain1 <- cbind(ActivityTrainIdx["Activity"], XTrain0)
-XTrain2 <- cbind(SubjectTrainFile, XTrain1)
+XTrain1 <- cbind(zz, XTrain0)
 
 ##  merge the two data sets
 
-FinalData <- rbind(XTest2, XTrain2)
+FinalData <- rbind(XTest1, XTrain1)
 
 
 ##  gather, then separate
 
 xx <- FinalData %>%
   gather(Measure, Measurement, `tBodyAcc-std()-X`:`fBodyGyro-mean()-Z`)
-yy <- xx %>%
-  separate(Measure, into=c("ActType", "MeasureType", "Axis"), sep ="\\-")
-zz <- yy %>%
-  group_by(Subject, Activity, ActType, MeasureType, Axis)
-qq <- zz %>%
-  summarize(AvgMeasure = mean(Measurement))
-
 rr <- xx %>%
   group_by(Subject, Activity, Measure)
 ss <- rr %>%
   summarize(AvgMeasure = mean(Measurement))
 
-qq <- arrange(qq, Subject, Activity, ActType, MeasureType, Axis)
 ss <- arrange(ss, Subject, Activity, Measure)
 
 ##    Initial Cleanup
@@ -116,12 +114,14 @@ unlink("UCI HAR Dataset", recursive = TRUE)
 
 ##    Write output file
 
-write.table(qq, file = "SamsungFile.txt", sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
-write.table(ss, file = "SamsungFile2.txt", sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(ss, file = "SamsungFile.txt", sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 ##    Remove objects from environment
 
 ##    rm(list=ls())
+##    unloadNamespace("tidyr")
+##    unloadNamespace("dplyr")
+##    unloadNamespace("plyr")
 
 print("End of Script")
 
@@ -129,3 +129,5 @@ print("End of Script")
 ##    Check your directory for the file "SamsungFile.txt
 
 ##    End
+
+
