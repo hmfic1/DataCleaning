bebdf866e9bb0bf6d2cc8fbcfc643d8ca98e0551
hmfic1 <jimrmck@gmail.com>
2016-02-27 18:15:55 -0800

    somewhat finished 2016 02 27

--- README.md
+++ README.md
@@ -2,13 +2,31 @@
 ## Final Project
 ### February, 2016
 
+This project takes data 
+
 
 Before running the run_analysis.R script:
   
-* ensure that you have an internet connection established
-* open R or R Studio
-* set your working directory to the directory of your choice
-    + This script will download a zip file to your current working directory
-* ensure that you have the following packages installed: plyr, dplyr, tidyr
-* open the run_analysis.R script in R or R Studio
+* Ensure that you have an internet connection established
+* Open R Studio
+* Ensure that you have the following packages installed: plyr, dplyr, tidyr
+* Set your working directory to the directory of your choice
+    + This script will download a zip file to your working directory
+    + It will then unzip the file
+* Open the run_analysis.R script in R Studio
 
+
+The script cleans up after itself. Data files downloaded (and unzipped) are eventually deleted. Additionally, the environment is cleaned of objects by the use of the rm(list=ls()) command at the end of the script.
+
+
+
+
+Citation:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 ##  run_analysis.R
 
-##    load appropriate packages
+##    load appropriate R packages
 
 library(plyr) 
 library(dplyr)
 library(tidyr)
 
-##    delete any previous files and directories
+##    To start off clean, delete any previous files and directories
 
 if(file.exists("SamsungFile.txt")) {
   file.remove("SamsungFile.txt")
@@ -25,9 +25,9 @@ if(dir.exists("UCI HAR Dataset")) {
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "data.zip")
 unzip("data.zip")
 
-##    read files into datasets
+##    Read files into datasets in current working directory
 
-features <-read.table("./UCI HAR Dataset/features.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 XTestfile <- read.table("./UCI HAR Dataset/test/X_test.txt", strip.white = TRUE)
@@ -38,14 +38,17 @@ XTrainfile <- read.table("./UCI HAR Dataset/train/X_train.txt", strip.white = TR
 SubjectTrainFile <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 ActivityTrainFile <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
-##     get columns and column names for mean() and std()
+##    We are only getting the variables for mean (mean) and standard deviation (std) 
+##    Determine columns and get the column names for mean() and std() variables
 
 stdColumns <- grep(".std\\().", features$V2)
 stdColumnNames <- grep(".std\\().", features$V2, value = TRUE)
 meanColumns <- grep(".mean\\().", features$V2)
 meanColumnNames <- grep(".mean\\().", features$V2, value = TRUE)
 
-##     subset test and training datasets, getting only the mean() and std() columns, and replace variable names
+##     Now we know which columns ("stdColumns" and "meanColumns") have the data we want to evaluate
+##     Subset test and training datasets ("Xtestfile" and "Xtrainfile"), getting only the mean() and std() columns 
+##     Then, replace replace variable names using the colnames function
 
 XTestSelectedstd <- XTestfile[,stdColumns]
 colnames(XTestSelectedstd) <- stdColumnNames
@@ -59,7 +62,9 @@ colnames(XTrainSelectedstd) <- stdColumnNames
 XTrainSelectedmean <- XTrainfile[,meanColumns]
 colnames(XTrainSelectedmean) <- meanColumnNames
 
-#    
+##    The Subject and Activity datasets have the information about which test subject and which activity were performed
+##    cbind the subject and activity data, replace the variable names, and replace the activity values with descriptive names
+##    Because we're doing this for both the test data and the training data, we'll perform the activities twice
 
 yy <- cbind(SubjectTestFile, ActivityTestFile)
 colnames(yy) <- c("Subject", "Activity")
@@ -72,9 +77,6 @@ yy$Activity[yy$Activity == "4"] <- "SITTING"
 yy$Activity[yy$Activity == "5"] <- "STANDING"
 yy$Activity[yy$Activity == "6"] <- "LAYING"
 
-XTest0 <- cbind(XTestSelectedstd, XTestSelectedmean)
-XTest1 <- cbind(yy, XTest0)
-
 zz <- cbind(SubjectTrainFile, ActivityTrainFile)
 colnames(zz) <- c("Subject", "Activity")
 
@@ -86,47 +88,51 @@ zz$Activity[zz$Activity == "4"] <- "SITTING"
 zz$Activity[zz$Activity == "5"] <- "STANDING"
 zz$Activity[zz$Activity == "6"] <- "LAYING"
 
+##     cbind the data for the std() and mean() extracted data, then add the descriptors (subject, activity to the front)
+
+XTest0 <- cbind(XTestSelectedstd, XTestSelectedmean)
+XTest1 <- cbind(yy, XTest0)
+
 XTrain0 <- cbind(XTrainSelectedstd, XTrainSelectedmean)
 XTrain1 <- cbind(zz, XTrain0)
 
-##  merge the two data sets
+##     Merge the two data sets; this will be the dataset from which we can use dplyr and tidyr to evaluate the data
 
 FinalData <- rbind(XTest1, XTrain1)
 
-
-##  gather, then separate
+##     Gather the data based on the measurements
 
 xx <- FinalData %>%
   gather(Measure, Measurement, `tBodyAcc-std()-X`:`fBodyGyro-mean()-Z`)
+
+##     Now group the data and summarize
+
 rr <- xx %>%
   group_by(Subject, Activity, Measure)
 ss <- rr %>%
   summarize(AvgMeasure = mean(Measurement))
 
+##     Sort the data by Subject, Activity, and Measure
+
 ss <- arrange(ss, Subject, Activity, Measure)
 
 ##    Initial Cleanup
 
-##    Delete downloaded files
+##    Delete downloaded files including the original downloaded zipped file and all the unzipped files
 
 file.remove("data.zip")
 unlink("UCI HAR Dataset", recursive = TRUE)
 
-##    Write output file
+##    Write output file, making sure to write character variables without quotes and to write data without headers
 
 write.table(ss, file = "SamsungFile.txt", sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 ##    Remove objects from environment
 
-##    rm(list=ls())
-##    unloadNamespace("tidyr")
-##    unloadNamespace("dplyr")
-##    unloadNamespace("plyr")
+rm(list=ls())
 
 print("End of Script")
-
-
-##    Check your directory for the file "SamsungFile.txt
+print("Check your directory for the file SamsungFile.txt")
 
 ##    End
 
